98411ef07dc60f8b6d760794cd4567bd25c42f7e
AnnStarrySky <anuta7267@mail.ru>
2026-02-28 17:24:21 +0700

    feat: resolve conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <Icon name="home" size={24} color="gray" />
         <Icon name="practice" size={24} color="gray" />
         <Icon name="profile" size={24} color="gray" />
-        
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
