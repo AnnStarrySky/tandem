661d7df1993b0faf90c7fbf320d0da5558d1d436
Yuriy Li <0yuriyli@gmail.com>
2026-04-17 15:08:50 +0300

    feat: add mock data to lesson endpoint

--- src/app/api/[locale]/lessons/route.ts
+++ src/app/api/[locale]/lessons/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { getMessages } from "next-intl/server";
+
+import { getPracticeTopicIdByLessonNumber } from "@/entities/practice";
+
 import type { Lesson } from "@shared/types";
 
 type Props = {
@@ -13,30 +17,53 @@ type BackendReturn = {
 }[];
 
 export async function GET(_request: NextRequest, { params }: Props): Promise<Response> {
-  const { locale } = (await params) ?? "en";
+  try {
+    const { locale } = (await params) ?? "en";
 
-  if (process.env.NEXT_PUBLIC_USE_MOCK === "true") {
-    //TODO: mock logic for lessons if needed
-  }
+    if (process.env.NEXT_PUBLIC_USE_MOCK === "true") {
+      const messages = await getMessages({ locale });
+      const topics = messages.Glossary.topics;
 
-  const BACKEND_URL = process.env.BACKEND_URL;
-  const url = new URL(`/api/${locale}/lessons/`, BACKEND_URL);
+      const result = new Array<Lesson>();
+      for (let i = 0; i < topics.length; i++) {
+        result.push({
+          id: topics[i].id,
+          name: getPracticeTopicIdByLessonNumber(i + 1) ?? "",
+          title: topics[i].title,
+          lessonNumber: i + 1,
+        });
+      }
 
-  const response = await fetch(url);
-
-  if (response.ok) {
-    const responseBody: BackendReturn = await response.json();
-    const result = new Array<Lesson>();
-    for (let i = 0; i < responseBody.length; i++) {
-      result.push({
-        id: responseBody[i].id,
-        name: responseBody[i].name,
-        title: responseBody[i].title,
-        lessonNumber: i + 1,
-      });
+      return NextResponse.json(result);
     }
-    return NextResponse.json(result);
-  } else {
-    return NextResponse.json({ success: false }, { status: response.status });
+
+    const BACKEND_URL = process.env.BACKEND_URL;
+    const url = new URL(`/api/${locale}/lessons/`, BACKEND_URL);
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const responseBody: BackendReturn = await response.json();
+      const result = new Array<Lesson>();
+      for (let i = 0; i < responseBody.length; i++) {
+        result.push({
+          id: responseBody[i].id,
+          name: responseBody[i].name,
+          title: responseBody[i].title,
+          lessonNumber: i + 1,
+        });
+      }
+      return NextResponse.json(result);
+    } else {
+      return NextResponse.json({ success: false }, { status: response.status });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: "Internal Server Error", message: error.message },
+        { status: 500 },
+      );
+    }
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
